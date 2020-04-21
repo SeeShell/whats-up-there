2123447dfb650a3cc4cc93988631d38be8626474
See Shell <323seeshell@gmail.com>
2020-04-21 14:05:39 -0700

    lint fixes AGAIN

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // Getting references to our form and input
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", function (event) {
+  signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
