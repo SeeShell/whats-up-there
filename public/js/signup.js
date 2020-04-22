2573edb9f51d63a11d80282b712f14ed7ef07a4e
Samwise <samuelfoster0@gmail.com>
2020-04-22 15:58:35 -0700

    changed up some of the css styles to make it darkmode.

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Getting references to our form and input
-  var signUpForm = $("form.signup");
+  var signUpForm = $("form#signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
