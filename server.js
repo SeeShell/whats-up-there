1c0ee5c9b1574ce5a8cf5cd81eaf58747644a6ca
jkaplanis <johntkaplanis@gmail.com>
2020-04-22 16:03:43 -0700

    adds get request for favorites, add association for models, update server.js, add seeds file

--- server.js
+++ server.js
@@ -23,9 +23,10 @@ app.use(passport.session());
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+require("./routes/favorites-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
