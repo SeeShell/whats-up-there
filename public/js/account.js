7bcbc9d586574bdf6c11a72ea3455a1b0b57e06c
Samwise <samuelfoster0@gmail.com>
2020-04-23 16:49:57 -0700

    working on the new ls of favorites.

--- public/js/account.js
+++ public/js/account.js
@@ -9,17 +9,32 @@ $(document).ready(function() {
   }
 
   function getFavorites() {
+    var list = $("<li>");
+    list.html(
+      `<a class="uk-accordion-title" href="#">Steve</a>
+        <div class="uk-accordion-content">
+        <p>10, satty 1, 02-24-2020</p>
+      </div>
+      <button class='uk-button delete-button'>remove</button>`
+    );
+    list.attr("data-id", data[i].id);
+    $("#favorites-list").append(list);
+
     $("#favorites-list-ul").empty();
-    $.get("/api/user_favorites").then(function(data) {
-      for (let i = 0; i < data.length; i++) {
-        var list = $("<li>");
-        list.text(
-          `${data[i].nickname} <button class='uk-button delete-button'>remove</button>`
-        );
-        list.attr("data-id", data[i].id);
-        $("#favorites-list").append(list);
-      }
-    });
+    // $.get("/api/user_favorites").then(function(data) {
+    //   for (let i = 0; i < data.length; i++) {
+    //     var list = $("<li>");
+    //     list.html(
+    //       `<a class="uk-accordion-title" href="#">${data[i].nickname}</a>
+    //         <div class="uk-accordion-content">
+    //         <p>${data[i].satID} ${data[i].satName} ${data[i].launchDate}</p>
+    //       </div>
+    //       <button class='uk-button delete-button'>remove</button>`
+    //     );
+    //     list.attr("data-id", data[i].id);
+    //     $("#favorites-list").append(list);
+    //   }
+    // });
 
     $("#delete").on("click", function() {
       const id = $(this)
