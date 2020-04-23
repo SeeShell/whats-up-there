5019971bc46f13eab5b6165d8b3d15154c64d90d
See Shell <323seeshell@gmail.com>
2020-04-23 10:26:26 -0700

    updated seeds and added ajax folder

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
   });
 });
+
+let userLon;
+let userLat;
+window.navigator.geolocation.getCurrentPosition(logLocation);
+
+function logLocation(position) {
+  const userLon = position.coords.longitude;
+  const userLat = position.coords.latitude;
+
+  localStorage.setItem("userLon", userLon);
+  localStorage.setItem("userLat", userLat);
+  return userLon, userLat;
+}
+console.log(userLon, userLat);
