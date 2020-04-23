b4bb771d803d499f89c3b0cd68ed44077567db6c
jkaplanis <johntkaplanis@gmail.com>
2020-04-22 20:28:32 -0700

    require express-handlebars in server, edit html routes to render pages with handlebars, edit main.handlebars to create proper stylesheet link.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,12 +5,18 @@ var path = require("path");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
+  const styleArray = [
+    { stylesheet: "stylesheets/signup-style.css" },
+    { stylesheet: "stylesheets/login-style.css" },
+    { stylesheet: "stylesheets/member-style.css" }
+  ];
+
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("signup", styleArray[0]);
   });
 
   app.get("/login", function(req, res) {
@@ -18,12 +24,12 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login", styleArray[1]);
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render("members", styleArray[2]);
   });
 };
