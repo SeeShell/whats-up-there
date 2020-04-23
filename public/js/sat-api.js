b5332200877bdbff9b26a85145f7ba1e60d2e1f9
jkaplanis <johntkaplanis@gmail.com>
2020-04-23 16:09:47 -0700

    add map data generators, update script tags for esri, add html file to test map display

--- public/js/sat-api.js
+++ public/js/sat-api.js
@@ -1,7 +1,6 @@
 const queryN2YO = "https://www.n2yo.com/rest/v1/satellite/";
 const apiKeyN2YO = "&apiKey=NWXNVK-K8V7UG-YTMY6D-4DCH";
 const userAlt = 0;
-let satID = 25544;
 
 window.satApi = {
   getPosition: (userLon, userLat) => {
@@ -17,19 +16,36 @@ window.satApi = {
   },
 
   getAbove: (userLon, userLat, categoryID) => {
-    let searchRadius = 45;
+    let searchRadius = 15;
     let aboveQuery = `/above/${userLat}/${userLon}/${userAlt}/${searchRadius}/${categoryID}`;
     $(function() {
       $.ajax({
         url: queryN2YO + aboveQuery + apiKeyN2YO,
         method: "GET"
       }).then(result => {
-        let aboveData = result.above.map(sat => {
-          return `${sat.satid}, ${sat.satname}, ${sat.launchDate}, ${sat.satlat}, ${sat.satlng}, ${sat.satalt}, \n`;
-        });
+        // let aboveData = result.above.map(sat => {
+        //   return `${sat.satid}, ${sat.satname}, ${sat.launchDate}, ${sat.satlat}, ${sat.satlng}, ${sat.satalt}, \n`;
+        // });
+        const aboveData = result.above;
         console.log(aboveData.length);
-        console.log(result);
-        return result, aboveData;
+        console.log(aboveData);
+        let points = "";
+        let pointGraphics = [];
+        for (let i = 0; i < aboveData.length; i++) {
+          points += `
+          const point${i} = new Point({
+            longitude: ${aboveData[i].satlng},
+            latitude: ${aboveData[i].satlat}
+          });
+          const pointGraphic${i} = new Graphic({
+            geometry: point${i},
+            symbol: textSymbol
+          });`;
+          pointGraphics += `pointGraphic${i}, `;
+        }
+        console.log(points);
+        console.log(pointGraphics);
+        return points;
       });
     });
   }
