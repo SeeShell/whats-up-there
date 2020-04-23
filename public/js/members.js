8ab4d5c668e4bcaa44ddb95db318dd70480e0c82
Charlotte Mountain <charr.mt@gmail.com>
2020-04-23 09:35:19 -0700

    link fixed

--- public/js/members.js
+++ public/js/members.js
@@ -8,24 +8,29 @@
 
 // CLOCK
 function showTime() {
-  var date = new Date();
-  var hr = date.getHours();
-  var min = date.getMinutes();
-  var sec = date.getSeconds();
+    var date = new Date();
+    var hr = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
 
-  if (min < 10) {
-    min = "0" + min;
-  }
-  if (sec < 10) {
-    sec = "0" + sec;
-  }
+    if (min < 10) {
+        min = "0" + min;
+    }
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
 
-  var time = hr + ":" + min + ":" + sec;
-  document.getElementById("time").innerText = time;
-  document.getElementById("time").textContent = time;
-  setTimeout(showTime, 1000);
+    var time = hr + ":" + min + ":" + sec;
+    document.getElementById("time").innerText = time;
+    document.getElementById("time").textContent = time;
+    setTimeout(showTime, 1000);
 }
 showTime();
 
+// CITY
 var city = $("<p>").text("SAN DIEGO");
 $("#favorite-city").append(city);
+
+// USER
+var user = $("<p>").text("Welcome up, John");
+$("#user").append(user);
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,31 +2,31 @@
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  const styleArray = [
-    { stylesheet: "stylesheets/signup-style.css" },
-    { stylesheet: "stylesheets/login-style.css" },
-    { stylesheet: "stylesheets/member-style.css" }
-  ];
+    const styleArray = [
+        { stylesheet: "stylesheets/signup-style.css" },
+        { stylesheet: "stylesheets/login-style.css" },
+        { stylesheet: "stylesheets/member-style.css" }
+    ];
 
-  app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.render("signup", styleArray[0]);
-  });
+    app.get("/", function(req, res) {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            res.redirect("/members");
+        }
+        res.render("signup", styleArray[0]);
+    });
 
-  app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.render("login", styleArray[1]);
-  });
+    app.get("/login", function(req, res) {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            res.redirect("/members");
+        }
+        res.render("login", styleArray[1]);
+    });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.render("members", styleArray[2]);
-  });
+    // Here we've add our isAuthenticated middleware to this route.
+    // If a user who is not logged in tries to access this route they will be redirected to the signup page
+    app.get("/members", isAuthenticated, function(req, res) {
+        res.render("members", styleArray[2]);
+    });
 };
