0c160f30989c0fd85a88782da02a0f2b5bd58a9f
See Shell <323seeshell@gmail.com>
2020-04-23 12:20:06 -0700

    renamed and moved sat-api to public folders for access via members page

---
+++ public/js/sat-api.js
@@ -0,0 +1,37 @@
+const queryN2YO = "https://www.n2yo.com/rest/v1/satellite/";
+const apiKeyN2YO = "&apiKey=NWXNVK-K8V7UG-YTMY6D-4DCH";
+const userAlt = 0;
+let satID = 25544;
+
+window.satApi = {
+  getPosition: (userLon, userLat) => {
+    let positionQuery = `/positions/${satID}/${userLat}/${userLon}/${userAlt}/50`;
+    $(function() {
+      $.ajax({
+        url: queryN2YO + positionQuery + apiKeyN2YO,
+        method: "GET"
+      }).then(result => {
+        console.log(result);
+      });
+    });
+  },
+
+  getAbove: (userLon, userLat) => {
+    let searchRadius = 45;
+    let categoryID = 0;
+    let aboveQuery = `/above/${userLat}/${userLon}/${userAlt}/${searchRadius}/${categoryID}`;
+    $(function() {
+      $.ajax({
+        url: queryN2YO + aboveQuery + apiKeyN2YO,
+        method: "GET"
+      }).then(result => {
+        // console.log(result);
+        let aboveData = result.above.map(sat => {
+          return `${sat.satid}, ${sat.satname}, ${sat.launchDate}, ${sat.satlat}, ${sat.satlng}, ${sat.satalt}, \n`;
+        });
+        console.log(aboveData.length);
+        return aboveData;
+      });
+    });
+  }
+};
