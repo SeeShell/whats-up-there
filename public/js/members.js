0802eee7b849a3ded07ef7d4d2e9199b603e57b5
Charlotte Mountain <charr.mt@gmail.com>
2020-04-22 22:05:00 -0700

    tests run

--- public/js/members.js
+++ public/js/members.js
@@ -8,23 +8,22 @@
 
 // CLOCK
 function showTime() {
-    var date = new Date();
-    var hr = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
+  var date = new Date();
+  var hr = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
 
-    if (min < 10) {
-        min = "0" + min;
-    }
-    if (sec < 10) {
-        sec = "0" + sec;
-    }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
 
-
-    var time = hr + ":" + min + ":" + sec;
-    document.getElementById("time").innerText = time;
-    document.getElementById("time").textContent = time;
-    setTimeout(showTime, 1000);
+  var time = hr + ":" + min + ":" + sec;
+  document.getElementById("time").innerText = time;
+  document.getElementById("time").textContent = time;
+  setTimeout(showTime, 1000);
 }
 showTime();
 
