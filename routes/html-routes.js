b8b8c797a5ea88a4cdda1c7801dca5ac4e399789
jkaplanis <johntkaplanis@gmail.com>
2020-04-22 20:32:30 -0700

    remove path required in html routes js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,3 @@
-// Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
-
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
