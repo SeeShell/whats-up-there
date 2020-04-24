b8ed421c57f768d82307c7f489f481247cfd2eb4
jkaplanis <johntkaplanis@gmail.com>
2020-04-24 16:34:31 -0700

    add logic to generate map with satellites

--- public/js/satmap.js
+++ public/js/satmap.js
@@ -39,7 +39,6 @@
 //     // return mapCoords;
 //   });
 // });
-let mapCoords = "";
 userLat = parseFloat(localStorage.getItem("userLat"));
 userLon = parseFloat(localStorage.getItem("userLon"));
 // const userAlt = 0;
@@ -53,16 +52,16 @@ satApi.getAbove(userLon, userLat, 0, 15);
 // });
 function sendAnswers(data) {
   console.log(data);
-  mapCoords = data;
-  mapCoords.map(sat => {
+  const mapCoords = data.map(sat => {
     return {
       latitude: sat.satlat,
       longitude: sat.satlng
     };
   });
+
+  initSatMap(mapCoords);
 }
 
-sendAnswers(1);
 //       .then(mapCoords => {
 //         const mapProps = {
 //           center: { userLat, userLon }, // get from local storage
@@ -75,63 +74,64 @@ sendAnswers(1);
 //   });
 // });
 
-// function initSatMap({ center, points }) {
-//   require([
-//     "esri/Map",
-//     "esri/PopupTemplate",
-//     "esri/views/MapView",
-//     "esri/Graphic",
-//     "esri/geometry/Point"
-//   ], initEsriMapView);
+function initSatMap(mapCoords) {
+  require([
+    "esri/Map",
+    "esri/PopupTemplate",
+    "esri/views/MapView",
+    "esri/Graphic",
+    "esri/geometry/Point"
+  ], initEsriMapView);
 
-//   // arcGIS esri map
-//   function initEsriMapView(Map, PopupTemplate, MapView, Graphic, Point) {
-//     const map = new Map({
-//       basemap: "satellite"
-//     });
+  // arcGIS esri map
+  function initEsriMapView(Map, PopupTemplate, MapView, Graphic, Point) {
+    const map = new Map({
+      basemap: "satellite"
+    });
 
-//     const view = new MapView({
-//       center: new Point(center),
-//       container: "viewDiv",
-//       map: map,
-//       zoom: 4
-//     });
+    const view = new MapView({
+      center: [userLon, userLat],
+      container: "viewDiv",
+      map: map,
+      zoom: 4
+    });
 
-//     // Create a symbol for drawing the point
-//     const textSymbol = {
-//       type: "text", // autocasts as new TextSymbol()
-//       color: "#7A003C",
-//       text: "\ue680", // esri-icon-map-pin
-//       font: {
-//         // autocasts as new Font()
-//         size: 24,
-//         family: "CalciteWebCoreIcons"
-//       }
-//     };
+    // Create a symbol for drawing the point
+    const textSymbol = {
+      type: "text", // autocasts as new TextSymbol()
+      color: "#7A003C",
+      text: "\ue680", // esri-icon-map-pin
+      font: {
+        // autocasts as new Font()
+        size: 24,
+        family: "CalciteWebCoreIcons"
+      }
+    };
 
-// const textSat = {
-//   type: "text", // autocasts as new TextSymbol()
-//   color: "white",
-//   haloColor: "black",
-//   haloSize: "1px",
-//   text: "sat name",
-//   xoffset: 3,
-//   yoffset: 3,
-//   font: {
-//     // autocasts as new Font()
-//     size: 12,
-//     family: "Josefin Slab",
-//     weight: "bold"
-//   }
-// };
+    // const textSat = {
+    //   type: "text", // autocasts as new TextSymbol()
+    //   color: "white",
+    //   haloColor: "black",
+    //   haloSize: "1px",
+    //   text: "sat name",
+    //   xoffset: 3,
+    //   yoffset: 3,
+    //   font: {
+    //     // autocasts as new Font()
+    //     size: 12,
+    //     family: "Josefin Slab",
+    //     weight: "bold"
+    //   }
+    // };
 
-//     const pointGraphics = points.map(point => {
-//       return new Graphic({
-//         geometry: new Point(point),
-//         symbol: textSymbol
-//       });
-//     });
+    const pointGraphics = mapCoords.map(point => {
+      return new Graphic({
+        geometry: new Point(point),
+        symbol: textSymbol
+      });
+    });
 
-//     // Add the graphics to the view's graphics layer
-//     view.graphics.addMany(pointGraphics);
-//   }
+    // Add the graphics to the view's graphics layer
+    view.graphics.addMany(pointGraphics);
+  }
+}
