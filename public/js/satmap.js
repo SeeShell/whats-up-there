15876c487b99fff66d49a2fed30d746392de1a90
jkaplanis <johntkaplanis@gmail.com>
2020-04-24 16:42:49 -0700

    remove old comments

--- public/js/satmap.js
+++ public/js/satmap.js
@@ -1,55 +1,9 @@
-// const mapProps = {
-//   center: { latitude: 32, longitude: -117 }, // get from local storage
-//   // get the points using satApi.getAbove
-//   points: [
-//     { latitude: 34.05, longitude: -117.19 },
-//     { longitude: -124.63, latitude: 41.88 }
-//   ]
-// };
-
-// const points = satApi.getAbove(userLon, userLat, 0, 15);
-
-// const mapProps = {
-//   center: { userLat, userLon }, // get from local storage
-//   // get the points using satApi.getAbove
-//   points: []
-// };
-// $(document).ready(function() {
-//   userLat = localStorage.getItem("userLon");
-//   userLon = localStorage.getItem("userLat");
-//   console.log(userLat, userLon);
-//   const queryN2YO = "https://www.n2yo.com/rest/v1/satellite/";
-//   const apiKeyN2YO = "&apiKey=NWXNVK-K8V7UG-YTMY6D-4DCH";
-//   const userAlt = 0;
-//   const searchRadiusHere = 15;
-//   const categoryID = 0;
-//   let aboveQuery = `/above/${userLat}/${userLon}/${userAlt}/${searchRadiusHere}/${categoryID}`;
-//   $(function() {
-//     $.ajax({
-//       url: queryN2YO + aboveQuery + apiKeyN2YO,
-//       method: "GET"
-//     }).then(result => {
-//       console.log(result.above);
-//       // let mapCoords = result.above.map(sat => {
-//       //   return {
-//       //     latitude: sat.satlat,
-//       //     longitude: sat.satlng
-//       //   };
-//     });
-//     // return mapCoords;
-//   });
-// });
 userLat = parseFloat(localStorage.getItem("userLat"));
 userLon = parseFloat(localStorage.getItem("userLon"));
-// const userAlt = 0;
-// const searchRadius = 15;
-// const categoryID = 0;
 console.log(userLat, userLon);
 
 satApi.getAbove(userLon, userLat, 0, 25);
-// .then(result => {
-//   console.log(result);
-// });
+
 function sendAnswers(data) {
   console.log(data);
   const mapCoords = data.map(sat => {
@@ -61,18 +15,6 @@ function sendAnswers(data) {
 
   initSatMap(mapCoords);
 }
-
-//       .then(mapCoords => {
-//         const mapProps = {
-//           center: { userLat, userLon }, // get from local storage
-//           // get the points using satApi.getAbove
-//           points: mapCoords
-//         };
-//         console.log(mapProps);
-//         initSatMap(mapProps);
-//       });
-//   });
-// });
 
 function initSatMap(mapCoords) {
   require([
