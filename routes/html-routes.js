a051cbb5998514b60b5061fe26657a88f23c13c4
Charlotte Mountain <charr.mt@gmail.com>
2020-04-24 14:46:39 -0700

    TRAVIS BROOOOO

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,41 +2,41 @@
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-    const styleArray = [
-        { stylesheet: "stylesheets/signup-style.css" },
-        { stylesheet: "stylesheets/login-style.css" },
-        { stylesheet: "stylesheets/member-style.css" },
-        { stylesheet: "https://js.arcgis.com/4.15/esri/themes/light/main.css" },
-        { stylesheet: "stylesheets/account-style.css" }
-    ];
+  const styleArray = [
+    { stylesheet: "stylesheets/signup-style.css" },
+    { stylesheet: "stylesheets/login-style.css" },
+    { stylesheet: "stylesheets/member-style.css" },
+    { stylesheet: "https://js.arcgis.com/4.15/esri/themes/light/main.css" },
+    { stylesheet: "stylesheets/account-style.css" }
+  ];
 
-    app.get("/", function(req, res) {
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            res.render("members", styleArray[2]);
-        }
-        res.render("signup", styleArray[0]);
-    });
+  app.get("/", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.render("members", styleArray[2]);
+    }
+    res.render("signup", styleArray[0]);
+  });
 
-    app.get("/login", function(req, res) {
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            res.render("members", styleArray[2]);
-        }
-        res.render("login", styleArray[1]);
-    });
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.render("members", styleArray[2]);
+    }
+    res.render("login", styleArray[1]);
+  });
 
-    // Here we've add our isAuthenticated middleware to this route.
-    // If a user who is not logged in tries to access this route they will be redirected to the signup page
-    app.get("/members", isAuthenticated, function(req, res) {
-        res.render("members", styleArray[2]);
-    });
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.render("members", styleArray[2]);
+  });
 
-    app.get("/maps", isAuthenticated, function(req, res) {
-        res.render("map"), styleArray[3];
-    });
+  app.get("/maps", isAuthenticated, function(req, res) {
+    res.render("map"), styleArray[3];
+  });
 
-    app.get("/account", isAuthenticated, function(req, res) {
-        res.render("account", styleArray[4]);
-    });
+  app.get("/account", isAuthenticated, function(req, res) {
+    res.render("account", styleArray[4]);
+  });
 };
