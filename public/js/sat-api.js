146b98f637c615f6e4a8b560f18cb8fe62a21583
Samwise <samuelfoster0@gmail.com>
2020-04-24 11:58:51 -0700

    added a logo to the header texts, made a loading text, and made a spinning style.

--- public/js/sat-api.js
+++ public/js/sat-api.js
@@ -143,6 +143,7 @@ function getAboveHomePage(userLon, userLat, categoryID) {
       url: queryN2YO + aboveQuery + apiKeyN2YO,
       method: "GET"
     }).then(result => {
+      $("#spinner").hide();
       const aboveDataHome = result.above;
       console.log(aboveDataHome);
 
