f8073ec1239d637cd79e6e48a5472bc4fcf0eccc
Charlotte Mountain <charr.mt@gmail.com>
2020-04-25 12:21:13 -0700

    forgot about travis

--- public/js/sat-api.js
+++ public/js/sat-api.js
@@ -57,69 +57,69 @@ const userAlt = 0;
 //   36: "Yaogan"
 // };
 window.satApi = {
-    getPosition: (userLon, userLat, satID) => {
-        let positionQuery = `/positions/${satID}/${userLat}/${userLon}/${userAlt}/50`;
-        $(function() {
-            $.ajax({
-                url: queryN2YO + positionQuery + apiKeyN2YO,
-                method: "GET"
-            }).then(result => {
-                console.log(result);
-            });
-        });
-    },
-    getAbove: (userLon, userLat, categoryID, searchRadius, source) => {
-        // let searchRadius = 15;
-        let aboveQuery = `/above/${userLat}/${userLon}/${userAlt}/${searchRadius}/${categoryID}`;
-        $(function() {
-            $.ajax({
-                url: queryN2YO + aboveQuery + apiKeyN2YO,
-                method: "GET"
-            }).then(result => {
-                // console.log(result.above);
-                let sats = result.above;
-                if (source === "members") {
-                    getAboveHomePage(sats, 15);
-                } else if (source === "maps") {
-                    sendAnswers(sats);
-                } else if (source === "category") {
-                    // showCategory(sats);
-                    getAboveHomePage(sats, 45);
-                }
-            });
-            // window.satApi.getVisualPass(userLat, userLon, 25544, userAlt, 2, 100);
-        });
-    },
-    getVisualPass: (userLat, userLon, satID, userAlt, days, minVisSeconds) => {
-        const visPassQuery = `/visualpasses/${satID}/${userLat}/${userLon}/${userAlt}/${days}/${minVisSeconds}`;
-        $(function() {
-            $.ajax({
-                url: queryN2YO + visPassQuery + apiKeyN2YO,
-                method: "GET"
-            }).then(result => {
-                console.log(result);
-            });
-        });
-    }
+  getPosition: (userLon, userLat, satID) => {
+    let positionQuery = `/positions/${satID}/${userLat}/${userLon}/${userAlt}/50`;
+    $(function() {
+      $.ajax({
+        url: queryN2YO + positionQuery + apiKeyN2YO,
+        method: "GET"
+      }).then(result => {
+        console.log(result);
+      });
+    });
+  },
+  getAbove: (userLon, userLat, categoryID, searchRadius, source) => {
+    // let searchRadius = 15;
+    let aboveQuery = `/above/${userLat}/${userLon}/${userAlt}/${searchRadius}/${categoryID}`;
+    $(function() {
+      $.ajax({
+        url: queryN2YO + aboveQuery + apiKeyN2YO,
+        method: "GET"
+      }).then(result => {
+        // console.log(result.above);
+        let sats = result.above;
+        if (source === "members") {
+          getAboveHomePage(sats, 15);
+        } else if (source === "maps") {
+          sendAnswers(sats);
+        } else if (source === "category") {
+          // showCategory(sats);
+          getAboveHomePage(sats, 45);
+        }
+      });
+      // window.satApi.getVisualPass(userLat, userLon, 25544, userAlt, 2, 100);
+    });
+  },
+  getVisualPass: (userLat, userLon, satID, userAlt, days, minVisSeconds) => {
+    const visPassQuery = `/visualpasses/${satID}/${userLat}/${userLon}/${userAlt}/${days}/${minVisSeconds}`;
+    $(function() {
+      $.ajax({
+        url: queryN2YO + visPassQuery + apiKeyN2YO,
+        method: "GET"
+      }).then(result => {
+        console.log(result);
+      });
+    });
+  }
 };
 
 function getAboveHomePage(sats, searchRad) {
-    numSats = sats.length;
-    numSatsMessage = `<p>There are ${numSats} satellites above you in a ${searchRad}&#176; search radius</p>`;
-    $("#num-sats").html(numSatsMessage);
-    //.append
-    const aboveDataHome = sats;
-    // console.log(aboveDataHome);
-    $("#spinner").hide();
-    $("#satellite-display").empty();
-    var userID = "";
-    $.get("/api/user_data").then(function(data) {
-        userID = data.id;
-    });
-    for (let i = 0; i < aboveDataHome.length; i++) {
-        var percentage1 = Math.floor(Math.random() * 100) + 1; //40
-        var percentage2 = Math.floor(Math.random() * 100) + 1; //20
-        var markerLink = `<a href="#satellite${i +
+  numSats = sats.length;
+  numSatsMessage = `<p>There are ${numSats} satellites above you in a ${searchRad}&#176; search radius</p>`;
+  $("#num-sats").html(numSatsMessage);
+  //.append
+  const aboveDataHome = sats;
+  // console.log(aboveDataHome);
+  $("#spinner").hide();
+  $("#satellite-display").empty();
+  var userID = "";
+  $.get("/api/user_data").then(function(data) {
+    userID = data.id;
+  });
+  for (let i = 0; i < aboveDataHome.length; i++) {
+    var percentage1 = Math.floor(Math.random() * 100) + 1; //40
+    var percentage2 = Math.floor(Math.random() * 100) + 1; //20
+    var markerLink = `<a href="#satellite${i +
       1}" uk-toggle class="uk-position-absolute uk-transform-center" style="left: 
           ${i + percentage1 + 5}%; top: 
           ${i + percentage2 + 5}%" href="#" uk-marker>
@@ -141,52 +141,52 @@ function getAboveHomePage(sats, searchRad) {
         <br>
         <a href="/maps"> VIEW MAP</a>
         </div> </div>`;
-        $("#satellite-display").append(markerLink);
-    }
-    $(".sat").on("click", function(event) {
-        var satellite = $(this);
-        $(this).hide();
-        event.preventDefault();
-        satData = {
-            satName: satellite.attr("data-name"),
-            satID: satellite.attr("data-id"),
-            nickname: satellite.attr("data-name"),
-            UserId: userID
-        };
-        $.post("/api/user_favorites", satData).then(function() {
-            getFavorites();
-        });
+    $("#satellite-display").append(markerLink);
+  }
+  $(".sat").on("click", function(event) {
+    var satellite = $(this);
+    $(this).hide();
+    event.preventDefault();
+    satData = {
+      satName: satellite.attr("data-name"),
+      satID: satellite.attr("data-id"),
+      nickname: satellite.attr("data-name"),
+      UserId: userID
+    };
+    $.post("/api/user_favorites", satData).then(function() {
+      getFavorites();
     });
+  });
 }
 getFavorites();
 
 function getFavorites() {
-    $("#fav-satellite").empty();
-    $.get("/api/user_favorites").then(function(data) {
-        for (let i = 0; i < data.length; i++) {
-            var satFav = $("<p>");
-            satFav.text(`${data[i].nickname}`);
-            let visPass = $("<button>");
-            // visPass.addClass("fave-visPass");
-            visPass.html(
-                `<a id='${data[i].satID}' class='uk-margin-small-left fave-visPass' uk-icon='rss'></a>`
-            );
-            // visPass.attr("id", data[i].satID);
-            satFav.append(visPass);
-            $("#fav-satellite").append(satFav);
-        }
-        $(".fave-visPass").on("click", function(event) {
-            event.preventDefault();
-            var sat = $(this);
-            let satID = sat.attr("id");
-            console.log(satID);
-            window.satApi.getVisualPass(userLat, userLon, satID, userAlt, 2, 100);
-        });
+  $("#fav-satellite").empty();
+  $.get("/api/user_favorites").then(function(data) {
+    for (let i = 0; i < data.length; i++) {
+      var satFav = $("<p>");
+      satFav.text(`${data[i].nickname}`);
+      let visPass = $("<button>");
+      // visPass.addClass("fave-visPass");
+      visPass.html(
+        `<a id='${data[i].satID}' class='uk-margin-small-left fave-visPass' uk-icon='rss'></a>`
+      );
+      // visPass.attr("id", data[i].satID);
+      satFav.append(visPass);
+      $("#fav-satellite").append(satFav);
+    }
+    $(".fave-visPass").on("click", function(event) {
+      event.preventDefault();
+      var sat = $(this);
+      let satID = sat.attr("id");
+      console.log(satID);
+      window.satApi.getVisualPass(userLat, userLon, satID, userAlt, 2, 100);
     });
+  });
 }
 
 function showCategory(sats) {
-    console.log(sats);
-    const numSats = sats.length;
-    // $("#num-sat").append(``)
+  console.log(sats);
+  const numSats = sats.length;
+  // $("#num-sat").append(``)
 }
