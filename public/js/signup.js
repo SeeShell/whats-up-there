b08410a81cff42ea1d7b736da5e508b3b7883b5a
Samwise <samuelfoster0@gmail.com>
2020-04-26 11:49:17 -0700

    error message now properly displays.

--- public/js/signup.js
+++ public/js/signup.js
@@ -36,7 +36,8 @@ $(document).ready(function() {
   }
 
   function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
+    console.log(err);
+    // $("#alert .msg").text(err.responseJSON);
     $("#alert").fadeIn(500);
   }
 });
