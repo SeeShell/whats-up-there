19ea08342331c478a2a2c2f29d84f3c1f2a709f1
jkaplanis <johntkaplanis@gmail.com>
2020-04-26 20:37:07 -0700

    add n2yo key to env file and replace in public code with process.env.N2YO_KEY

--- public/js/members.js
+++ public/js/members.js
@@ -12,7 +12,7 @@ function logLocation(position) {
 }
 
 function showCity(userLat, userLon) {
-  var APIKey = "82fdd99a86105b66de45ae6fa55be58f";
+  var APIKey = process.env.N2YO_KEY;
   var queryURL =
     "https://api.openweathermap.org/data/2.5/weather?lat=" +
     userLat +
